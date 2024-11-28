e3856fb102b45d8fec7cf73ce31efee1110f7bb5
Vertueux <81981323+vertueux@users.noreply.github.com>
2024-11-28 19:57:51 +0100

    Updated index.html & app.js.

--- js/app.js
+++ js/app.js
@@ -121,21 +121,3 @@ function searchTableEnter(event) {
         })
     }
 }
-
-async function fetchFiles () {
-  const response = await fetch('../pdf/list-files.json')
-  const files = await response.json()
-  const list = document.getElementById('file-list')
-  files.forEach(file => {
-    if (file.endsWith('.pdf')) {
-      const listItem = document.createElement('li')
-      const link = document.createElement('a')
-      link.href = `../pdf/${file}`
-      link.textContent = file
-      link.target = '_blank'
-      listItem.appendChild(link)
-      list.appendChild(listItem)
-    }
-  })
-}
-fetchFiles()
